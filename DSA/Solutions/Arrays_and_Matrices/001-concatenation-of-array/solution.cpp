2765ebdec52d09e7c741c31813f7f2f45add004a
PrasanKumar1101 <PrasanKumar1101@users.noreply.github.com>
2026-05-30 21:52:19 +0530

    solve: 001-concatenation-of-array/solution.cpp [Accepted, 3 ms]

--- DSA/Solutions/Arrays_and_Matrices/001-concatenation-of-array/solution.cpp
+++ DSA/Solutions/Arrays_and_Matrices/001-concatenation-of-array/solution.cpp
@@ -8,8 +8,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans(nums);
-        ans.insert(ans.end(), nums.begin(), nums.end());
+        int n=nums.size();
+        vector<int>ans(n*2);
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
         return ans;
     }
 };
